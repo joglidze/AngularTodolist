425f95b53dd4accb46d1675e5271dd675b15c54c
luca <lucajoglidze@gmail.com>
2022-11-21 21:34:30 +0400

    project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'threedolist'`, () => {
+  it(`should have as title 'toDoList'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('threedolist');
+    expect(app.title).toEqual('toDoList');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('threedolist app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('toDoList app is running!');
   });
 });
